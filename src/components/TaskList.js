d6cd9b8f0e39943ad877a7c39e0215fbe9ef1505
Nguyễn Hải Triều <36495153+Czer0C@users.noreply.github.com>
2021-04-10 22:43:05 +0700

    Compostite components

---
+++ src/components/TaskList.js
@@ -0,0 +1,60 @@
+import React from "react";
+import Task from "./Task";
+import PropTypes from "prop-types";
+import LoadingRow from "./LoadingRow";
+export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
+  const events = {
+    onPinTask,
+    onArchiveTask,
+  };
+
+  if (loading) {
+    return (
+      <div className="list-items">
+        {[...Array(6)].map((i, index) => (
+          <LoadingRow key={`row-${index}`} />
+        ))}
+      </div>
+    );
+  }
+
+  if (tasks.length === 0) {
+    return (
+      <div className="list-items">
+        <div className="wrapper-message">
+          <span className="icon-check" />
+          <div className="title-message">You have no tasks</div>
+          <div className="subtitle-message">Sit back and relax</div>
+        </div>
+      </div>
+    );
+  }
+
+  const tasksInOrder = [
+    ...tasks.filter((t) => t.state === "TASK_PINNED"),
+    ...tasks.filter((t) => t.state !== "TASK_PINNED"),
+  ];
+
+  return (
+    <div className="list-items">
+      {tasksInOrder.map((task) => (
+        <Task key={task.id} task={task} {...events} />
+      ))}
+    </div>
+  );
+}
+
+TaskList.propTypes = {
+  /** Checks if it's in loading state */
+  loading: PropTypes.bool,
+  /** The list of tasks */
+  tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
+  /** Event to change the task to pinned */
+  onPinTask: PropTypes.func,
+  /** Event to change the task to archived */
+  onArchiveTask: PropTypes.func,
+};
+
+TaskList.defaultProps = {
+  loading: false,
+};
